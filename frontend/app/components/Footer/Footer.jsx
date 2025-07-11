c0c6d31f0a416319eed6bb0d81ca65d6e314a20d
Zondkun <100234066+Zondkun@users.noreply.github.com>
2025-07-11 20:56:04 +0500

    Changed navigation

--- frontend/app/components/Footer/Footer.jsx
+++ frontend/app/components/Footer/Footer.jsx
@@ -1,34 +1,59 @@
 import { montserrat } from '@/lib/fonts'
+import { inter } from '@/lib/fonts'
+import Link from 'next/link'
 
 export default function Footer() {
   return (
       <div className="w-screen h-53.5" style={{ backgroundColor: "#F6FFDE" }}>
-          <footer className="flex w-[1400px] pt-10 pb-4 mx-auto h-full justify-between">
-              <div className="flex flex-col justify-between">
-                  <div className="flex flex-col gap-2">
+          <footer className={`${montserrat.className} flex w-[1400px] pt-10 pb-4 mx-auto h-full justify-between`} style={{color: '#040404'}}>
+              <div className="flex flex-col justify-between mr-[5%]">
+                  <div className={`font-semibold text-lg flex flex-col gap-2`}>
                       <p>+7 3822 71-67-69</p>
                       <p>perspectiva@education70.ru</p>
                   </div>
-                  <p>© 2025 МАОУ Школа «Перспектива» г. Томск</p>
+                  <p className={`font-normal text-sm`}>© 2025 МАОУ Школа «Перспектива» г. Томск</p>
               </div>
-              <div className="flex flex-col gap-2 mr-[13%]">
-                  <p>О проекте</p>
-                  <p>Новости</p>
-                  <p>Каталог материалов</p>
+              <div className={`font-medium text-base flex flex-col gap-2 mr-[13%]`}>
+                  <nav>
+                      <ul>
+                          <li>
+                              <Link href="/about">О проекте</Link>
+                          </li>
+                          <li>
+                              <Link href="/news">Новости</Link>
+                          </li>
+                          <li>
+                              <Link href="/catalog">Каталог материалов</Link>
+                          </li>
+                      </ul>
+                  </nav>
               </div>
               <div className="flex flex-col justify-between mr-[15%]">
-                  <div className="flex flex-col gap-2">
-                      <p>Вопрос-ответ</p>
-                      <p>Контакты</p>
+                  <div className="text-base font-medium flex flex-col gap-2">
+                      <nav>
+                          <ul>
+                              <li><Link href="/qa">Вопрос-ответ</Link></li>
+                              <li><Link href="/contacts">Контакты</Link></li>
+                          </ul>
+                      </nav>
                   </div>
-                  <p>Designed by Freepik</p>
+                  <p className={`text-sm font-normal`}>Designed by Freepik</p>
               </div>
               <div className="flex flex-col justify-between">
-                  <div className="flex flex-col gap-2">
-                      <p>Пользовательское соглашение</p>
-                      <p>Политика конфиденциальности</p>
+                  <div className="text-base font-medium flex flex-col gap-2">
+                      <nav>
+                          <ul>
+                              <li><Link href="/terms">Пользовательское соглашение</Link></li>
+                              <li><Link href="/privacy">Политика конфиденциальности</Link></li>
+                          </ul>
+                      </nav>
                   </div>
-                  <p>Сделано в ... | TEAMCODE</p>
+                  <p className={`${inter.className} text-xs font-medium flex items-center gap-2`}>
+                      Сделано в
+                      <img src="/footer-images/logo.svg" alt="1t" className="w-[28.8px] h-6 inline-block" />
+                      <img src="/footer-images/union.svg" alt="Союз рф" className="w-[70px] h-[10.5px] inline-block" />
+                      | TEAMCODE
+                  </p>
               </div>
           </footer>
       </div>
