b940d8f877bc749f8d836547aa2f11cd49e33ecd
Zondkun <100234066+Zondkun@users.noreply.github.com>
2025-07-16 21:58:09 +0500

    Finished footer layout

--- frontend/app/components/Footer/Footer.jsx
+++ frontend/app/components/Footer/Footer.jsx
@@ -19,20 +19,21 @@ export default function Footer() {
       <div className={`${styles.bg} gap-10 flex flex-col w-screen`}>
           <footer className={`${montserrat.className} ${styles.fontColor} ${styles.footer} ${pageWidth >= 768 && 
           pageWidth < 1024 ? 'flex-wrap' : '' } flex mx-auto h-full`}>
-              <div className="flex flex-col justify-between mr-[7.75%]">
+              <div className={`${pageWidth >= 1400 ? 'mr-[5%]':''} flex flex-col justify-between`}>
                   <div className={`font-semibold text-lg flex flex-col gap-2`}>
                       <p>+7 3822 71-67-69</p>
                       <p>perspectiva@education70.ru</p>
                   </div>
               </div>
-              <div className={`font-medium text-base flex flex-col gap-2 mr-[13%]`}>
+              <div className={`${pageWidth >= 1400 ? 'mr-[13%]':''} ${pageWidth >= 768 && pageWidth < 1024 ? 'ml-[7.75%]':''} 
+               ${pageWidth >= 1024 && pageWidth < 1400 ? 'mr-[14%]':''} font-medium text-base flex flex-col gap-2 `}>
                   <nav>
                       <ul>
                           <li><Link href="/about">О проекте</Link></li>
                           <li><Link href="/news">Новости</Link></li>
                           <li><Link href="/catalog">Каталог материалов</Link></li>
                           {pageWidth !== null && (
-                          <div className={`${pageWidth > 1024 ? 'hidden' : ''}`}>
+                          <div className={`${pageWidth > 1399 ? 'hidden' : ''}`}>
                               <li><Link href="/news">Вопрос-ответ</Link></li>
                               <li><Link href="/catalog">Контакты</Link></li>
                           </div>
@@ -40,7 +41,7 @@ export default function Footer() {
                       </ul>
                   </nav>
               </div>
-              {pageWidth !== null && pageWidth > 1024 && (
+              {pageWidth !== null && pageWidth > 1399 && (
                   <div className="flex flex-col justify-between mr-[15%]">
                     <div className="text-base font-medium flex flex-col gap-2">
                       <nav>
@@ -63,15 +64,17 @@ export default function Footer() {
                   </div>
               </div>
           </footer>
-          <div className={`${pageWidth <= 360 ? 'flex-col gap-2 w-80 mx-auto' : ''} mx-auto flex ${montserrat.className}
-          ${styles.footer}`}>
-            <div className={`${pageWidth >= 768 && pageWidth < 1024 ? 'flex flex-col gap-2' : ''}`}>
-              <p className={`font-normal text-sm`}>
-                © 2025 МАОУ Школа {pageWidth <= 360 ? <br/> : ''} «Перспектива» г. Томск
+          <div className={`${styles.footerBottom} ${pageWidth < 768 ? 'flex-col':''} ${pageWidth <= 360 ? 'gap-2 w-80' : ''} mx-auto flex 
+          ${montserrat.className}`}>
+            <div className={`flex ${styles.footerAbout} ${pageWidth >= 768 && pageWidth < 1024 ? 'gap-2' : ''} gap-1`}>
+              <p className={`${pageWidth >= 1024 && pageWidth < 1400 ? 'mr-[2.4%] w-[301px]':''} shrink-0 font-normal text-sm`}>
+                © 2025 МАОУ Школа {pageWidth <= 360 || (pageWidth >= 1024 && pageWidth < 1400) ? <br/> : ''} «Перспектива» г. Томск
               </p>
-              <p className={`text-sm font-normal`}>Designed by Freepik</p>
+              <p className={`w-[145px] flex items-end 
+              shrink-0 text-sm font-normal`}>Designed by Freepik</p>
             </div>
-            <p className={`${inter.className} ${pageWidth >= 768 && pageWidth < 1024 ? '' : '' } text-xs font-medium flex items-center gap-2`}>
+            <p className={`${inter.className} ${pageWidth >= 768 && pageWidth < 1024 ? 'ml-[5.6%]' : ''}
+             ${pageWidth >= 1400 ? 'ml-[15.5%]':''} text-xs font-medium flex items-center gap-2`}>
               Сделано в
               <img src="/footer-images/logo.svg" alt="1t" className="w-[28.8px] h-6 inline-block" />
               <img src="/footer-images/union.svg" alt="Союз рф" className="w-[70px] h-[10.5px] inline-block" />
