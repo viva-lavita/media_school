e57979c7ff353d7ad8e3675adf52c8c8c1ae64c4
Elena Frolova <frolova.est@gmail.com>
2025-09-01 01:01:08 +0700

    Новости доделываю ярлыки на конкурсах

--- frontend/app/news/page.js
+++ frontend/app/news/page.js
@@ -64,7 +64,7 @@ const newsData = [
     title: "Заголовок 10",
     date: "19 апреля 2025",
     preview: "Текст превью 10. Узнайте о новых открытиях в области науки и технологий.",
-    image: "/images/news1.png",
+    image: "/images/news.png",
   },
 ];
 const announcementsData = [
@@ -126,43 +126,121 @@ const announcementsData = [
     title: "Анонс 10",
     date: "19 апреля 2025",
     preview: "Текст превью 10. Узнайте о новых открытиях в области науки и технологий.",
-    image: "/images/news1.png",
+    image: "/images/news.png",
   },
-];
-
-const contestsData = [
-    {
-    title: "Конкурс 1",
+  {
+    title: "Анонс 1",
     date: "10 апреля 2025",
     preview: "В минувший четверг в нашей школе прошёл необычный и насыщенный день — к нам приехали студенты факультета журналистики, чтобы провести серию мастер-классов и творческих встреч для школьников.",
     image: "/images/news.png",
   },
   {
-    title: "Конкурс 2",
+    title: "Анонс 2",
     date: "11 апреля 2025",
+    preview: "Текст превью 2. Узнайте, как новые инновации меняют наше представление о будущем.",
+    image: "/images/news.png",
+  },
+  {
+    title: "Анонс 3",
+    date: "12 апреля 2025",
+    preview: "Текст превью 3. Присоединяйтесь к нам, чтобы узнать о самых интересных фактах из истории.",
+    image: "/images/news.png",
+  },
+  {
+    title: "Анонс 4",
+    date: "13 апреля 2025",
+    preview: "Текст превью 4. Мы обсудим последние достижения в области медицины и здоровья.",
+    image: "/images/news.png",
+  },
+  {
+    title: "Анонс 5",
+    date: "14 апреля 2025",
+    preview: "Текст превью 5. В этом выпуске мы рассмотрим новые тренды в мире искусства.",
+    image: "/images/news.png",
+  },
+  {
+    title: "Анонс 6",
+    date: "15 апреля 2025",
+    preview: "Текст превью 6. Узнайте, как экология и устойчивое развитие становятся приоритетами для бизнеса.",
+    image: "/images/news.png",
+  },
+  {
+    title: "Анонс 7",
+    date: "16 апреля 2025",
+    preview: "Текст превью 7. Мы расскажем о самых захватывающих событиях в мире спорта.",
+    image: "/images/news.png",
+  },
+  {
+    title: "Анонс 8",
+    date: "17 апреля 2025",
+    preview: "Текст превью 8. Откройте для себя новые горизонты путешествий и приключений.",
+    image: "/images/news.png",
+  },
+  {
+    title: "Анонс 9",
+    date: "18 апреля 2025",
+    preview: "Текст превью 9. Мы обсудим влияние социальных сетей на современное общество.",
+    image: "/images/news.png",
+  },
+  {
+    title: "Анонс 10",
+    date: "19 апреля 2025",
+    preview: "Текст превью 10. Узнайте о новых открытиях в области науки и технологий.",
+    image: "/images/news.png",
+  },  
+];
+
+const contestsData = [
+    {
+    title: "Конкурс 1",
+    date: "12 апреля 2025",
+    date_start: "10 апреля 2025",
+    date_end: "10 апреля 2025",
+    is_over: "true",
+    preview: "В минувший четверг в нашей школе прошёл необычный и насыщенный день — к нам приехали студенты факультета журналистики, чтобы провести серию мастер-классов и творческих встреч для школьников.",
+    image: "/images/news.png",
+  },
+  {
+    title: "Конкурс 2",
+    date: "12 апреля 2025",
+    date_start: "10 апреля 2025",
+    date_end: "10 апреля 2025",
+    is_over: "true",
     preview: "Текст превью 2. Узнайте, как новые инновации меняют наше представление о будущем.",
     image: "/images/news.png",
   },
   {
     title: "Конкурс 3",
     date: "12 апреля 2025",
+    date_start: "10 апреля 2025",
+    date_end: "10 апреля 2025",
+    is_over: "true",
     preview: "Текст превью 3. Присоединяйтесь к нам, чтобы узнать о самых интересных фактах из истории.",
     image: "/images/news.png",
   },
   {
     title: "Конкурс 4",
     date: "13 апреля 2025",
+    date_start: "10 апреля 2025",
+    date_end: "10 апреля 2025",
+    is_over: "true",
     preview: "Текст превью 4. Мы обсудим последние достижения в области медицины и здоровья.",
     image: "/images/news.png",
   },
   {
     title: "Конкурс 5",
     date: "14 апреля 2025",
+    date_start: "10 апреля 2025",
+    date_end: "10 апреля 2025",
+    is_over: "true",    
     preview: "Текст превью 5. В этом выпуске мы рассмотрим новые тренды в мире искусства.",
     image: "/images/news.png",
   },
   {
     title: "Конкурс 6",
+    date_start: "10 апреля 2025",
+    date_end: "10 апреля 2025",
+    is_over: "true",    
     date: "15 апреля 2025",
     preview: "Текст превью 6. Узнайте, как экология и устойчивое развитие становятся приоритетами для бизнеса.",
     image: "/images/news.png",
@@ -170,27 +248,39 @@ const contestsData = [
   {
     title: "Конкурс 7",
     date: "16 апреля 2025",
+    date_start: "10 апреля 2025",
+    date_end: "10 апреля 2025",
+    is_over: "true",
     preview: "Текст превью 7. Мы расскажем о самых захватывающих событиях в мире спорта.",
     image: "/images/news.png",
   },
   {
     title: "Конкурс 8",
     date: "17 апреля 2025",
+    date_start: "10 апреля 2025",
+    date_end: "10 апреля 2025",
+    is_over: "true",
     preview: "Текст превью 8. Откройте для себя новые горизонты путешествий и приключений.",
     image: "/images/news.png",
   },
   {
     title: "Конкурс 9",
     date: "18 апреля 2025",
+    date_start: "10 апреля 2025",
+    date_end: "10 апреля 2025",
+    is_over: "true",
     preview: "Текст превью 9. Мы обсудим влияние социальных сетей на современное общество.",
     image: "/images/news.png",
   },
-  {
-    title: "Конкурс 10",
-    date: "19 апреля 2025",
-    preview: "Текст превью 10. Узнайте о новых открытиях в области науки и технологий.",
-    image: "/images/news1.png",
-  },
+//   {
+//     title: "Конкурс 10",
+//     date: "19 апреля 2025",
+    // date_start: "10 апреля 2025",
+    // date_end: "10 апреля 2025",
+    // is_over: "true",
+//     preview: "Текст превью 10. Узнайте о новых открытиях в области науки и технологий.",
+//     image: "/images/news.png",
+//   },
 ];
 
 export default function NewsPage() {
@@ -237,29 +327,41 @@ const handleTabChange = (tab) => {
     <>
       <div className={`${styles.newsContainer}`}>
         <div className={`${styles.newsAll}`}>
-          <div className={`${styles.announcements} flex flex-col gap-10`}>
-            <h1 className={`${comfortaa.className} ${styles.aboutLearning}`}>
+          <div className={`${styles.announcements} flex flex-col gap-7`}>
+            <h1
+              className={`${comfortaa.className} ${styles.aboutLearning}`}
+              style={{ paddingBottom: "9px" }}
+            >
               Новости, анонсы и конкурсы
             </h1>
 
-            <div className="flex space-x-4 mb-4">
+            <div className="flex justify-center space-x-[30px]">
               <button
                 onClick={() => handleTabChange("news")}
-                className={`${currentTab === "news" ? "font-bold" : ""}`}
+                className={`${
+                  currentTab === "news" ? "border-b-2 border-[#3C4226]" : ""
+                } ${montserrat.className} w-[208px]`}
+                style={{ paddingBottom: "9px" }}
               >
                 Новости
               </button>
               <button
                 onClick={() => handleTabChange("announcements")}
                 className={`${
-                  currentTab === "announcements" ? "font-bold" : ""
-                }`}
+                  currentTab === "announcements"
+                    ? "border-b-2 border-[#3C4226]"
+                    : ""
+                } ${montserrat.className} tab-style w-[208px]`}
+                style={{ paddingBottom: "9px" }}
               >
                 Анонсы
               </button>
               <button
                 onClick={() => handleTabChange("contests")}
-                className={`${currentTab === "contests" ? "font-bold" : ""}`}
+                className={`${
+                  currentTab === "contests" ? "border-b-2 border-[#3C4226]" : ""
+                } ${montserrat.className} w-[208px]`}
+                style={{ paddingBottom: "9px" }}
               >
                 Конкурсы
               </button>
@@ -267,7 +369,42 @@ const handleTabChange = (tab) => {
 
             <Link href="/onenews" className={`${styles.newsCard}`}>
               {currentItems.map((item, index) => (
-                <div key={index} className={`flex flex-col gap-2`}>
+                <div key={index} className={`flex flex-col gap-2 relative`}>
+                  <div
+                    style={{
+                      position: "absolute",
+                      top: "12px", 
+                      right: "12px",
+                      backgroundColor: "white",
+                      borderRadius: "30px",
+                      gap: '8px',
+                      paddingTop: '4px',
+                      paddingRight: '8px',
+                      paddingBottom: '4px',
+                      paddingLeft: '8px',
+                      height: "28px",
+                      display:
+                        currentTab === "contests"
+                          ? "flex"
+                          : "none",
+                      alignItems: "center",
+                      justifyContent: "center",
+                      fontFamily: "Montserrat",
+                      fontWeight: 500,
+                      fontSize: "16px",
+                      lineHeight: "100%",
+                      letterSpacing: "0%",
+                      boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)", 
+                    }}
+                  >
+                    <img
+                      src="/images/Star_news.svg"
+                      alt="Star"
+                      style={{ marginRight: "5px" }}
+                    />
+                    c {item.date_start}
+                  </div>
+
                   <figure className={`flex flex-col gap-3`}>
                     <img src={item.image} alt="новости" />
                     <figcaption
@@ -301,18 +438,64 @@ const handleTabChange = (tab) => {
               ))}
             </Link>
           </div>
-          <div className="flex justify-center mt-4">
-            {Array.from({ length: totalPages }, (_, index) => (
-              <button
-                key={index}
-                onClick={() => setCurrentPage(index + 1)}
-                className={`mx-1 ${
-                  currentPage === index + 1 ? "font-bold" : ""
-                }`}
-              >
-                {index + 1}
-              </button>
-            ))}
+          <div className="flex justify-center">
+            {totalPages > 1 && (
+              <>
+                <button
+                  onClick={() => setCurrentPage(currentPage - 1)}
+                  disabled={currentPage === 1}
+                  className={` ${
+                    currentPage === 1 ? "opacity-0 cursor-not-allowed" : ""
+                  }`}
+                  style={{
+                    paddingRight: "12px",
+                  }}
+                >
+                  <img src="/images/ArrowLeftBlack.svg" alt="" />
+                </button>
+                {Array.from({ length: totalPages }, (_, index) => (
+                  <button
+                    key={index}
+                    onClick={() => setCurrentPage(index + 1)}
+                    style={{
+                      paddingBottom: "9px",
+                      paddingLeft: "auto",
+                      paddingRight: "auto",
+                      paddingTop: "9px",
+                      lineHeight: "19px",
+                      border:
+                        currentPage === index + 1
+                          ? "1.5px solid #3C4226"
+                          : "none",
+                      fontFamily: "Inter",
+                      fontWeight: 400,
+                      fontStyle: "normal",
+                      fontSize: "16px",
+                      lineHeight: "19px",
+                      letterSpacing: "0%",
+                      textAlign: "center",
+                      width: "39px",
+                    }}
+                  >
+                    {index + 1}
+                  </button>
+                ))}
+                <button
+                  onClick={() => setCurrentPage(currentPage + 1)}
+                  disabled={currentPage === totalPages}
+                  className={` ${
+                    currentPage === totalPages
+                      ? "opacity-50 cursor-not-allowed"
+                      : ""
+                  }`}
+                  style={{
+                    paddingLeft: "12px",
+                  }}
+                >
+                  <img src="/images/ArrowRightBlack.svg" alt="" />
+                </button>
+              </>
+            )}
           </div>
         </div>
       </div>
