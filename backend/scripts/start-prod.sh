bc71fa69e110258b719a71a3fdcfb6b68ad58e85
vi.vala.vi <sovestyaaaa@gmail.com>
2025-09-04 00:38:40 +0300

    change db version

--- backend/scripts/start-prod.sh
+++ backend/scripts/start-prod.sh
@@ -6,8 +6,8 @@ HOST=${HOST:-0.0.0.0}
 PORT=${PORT:-8000}
 
 uv sync  # TODO: догрузка dev зависимостей, убрать, когда на проде будет DEBUG=False
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput
+python manage.py makemigrations
+python manage.py migrate
+python manage.py collectstatic  --noinput
 
 exec gunicorn --bind "${HOST}:${PORT}" --workers ${GUNICORN_WORKERS} --threads ${GUNICORN_THREADS} --timeout ${GUNICORN_TIMEOUT} --reload config.wsgi:application
