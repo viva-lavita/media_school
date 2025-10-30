6eda2d6d106d8b0b533c5d964682aba6738ca097
vi.vala.vi <sovestyaaaa@gmail.com>
2025-10-30 10:18:49 +0300

    change static path for debug

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
@@ -17,7 +16,7 @@ if settings.ENABLE_DEBUG_TOOLBAR:
 
     urlpatterns += [path("debug/ ", include(debug_toolbar.urls))]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
