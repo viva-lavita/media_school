6a366592c16f0c78e4f4574650c78c7271ac8a50
Your Name <veronichka.danilina@mail.ru>
2025-11-16 23:45:46 +0400

    fead: отредактированы стили на странице о проекте

--- frontend/app/about/page.jsx
+++ frontend/app/about/page.jsx
@@ -1,16 +1,16 @@
 'use client';
-import AboutProjectSection from "./components/AboutProjectSection";
+import AboutProjectSection from './components/AboutProjectSection';
 import styles from './components/About.module.css';
-import Advantages from "../components/Advantages/Advantages";
-import WhatAreWeStudyingSection from "../components/WhatAreWeStudyingSection/WhatAreWeStudyingSection";
-import Experts from "../components/Experts/Experts";
+import Advantages from '../components/Advantages/Advantages';
+import WhatAreWeStudyingSection from '../components/WhatAreWeStudyingSection/WhatAreWeStudyingSection';
+import Experts from '../components/Experts/Experts';
 
 export default function About() {
-  return (
-    <div className={styles.wrap}>
-     <AboutProjectSection />
-     <Advantages />
-     <WhatAreWeStudyingSection />
-    </div>
-  );
-};
+ return (
+  <div className={styles.wrap}>
+   <AboutProjectSection />
+   <Advantages />
+   <WhatAreWeStudyingSection />
+  </div>
+ );
+}
